33618e9b32dce52d74eddf6091f03312f6565b5c
amitaeit <amitaeit@gmail.com>
2022-08-11 14:01:07 +0530

    Submitted Project

--- IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
+++ IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
@@ -1,19 +1,33 @@
-
-
-
-function initialize()
-{
+function initialize(){
     const numNodewatt=document.querySelectorAll(".wattField");
     const numNodeUsage=document.querySelectorAll(".usageField");
     const numUsage1=document.querySelectorAll(".usageField1");
     const numWatt1=document.querySelectorAll(".wattField1");
-    numUsage1[0].value=0;
-    numWatt1[0].value=0;
+
+    //initialize washroom bulbs
+    for(let i=0;i<numWatt1.length;i++){
+        numUsage1[i].value=10;
+        numWatt1[i].value=12;
+    }
 
     for(let i=0;i<numNodeUsage.length;i++)
     {
-        numNodeUsage[i].value=0;
-        numNodewatt[i].value=0;
+        if(numNodewatt[i].classList.contains("fan")){
+            numNodewatt[i].value=60;
+            numNodeUsage[i].value=20;
+        }
+        else if(numNodewatt[i].classList.contains("AC")){
+            numNodewatt[i].value=1500;
+            numNodeUsage[i].value=8;
+        }
+        else if(numNodewatt[i].classList.contains("tubelight")){
+            numNodewatt[i].value=40;
+            numNodeUsage[i].value=8;
+        }
+        else if(numNodewatt[i].classList.contains("bulb")){
+            numNodewatt[i].value=20;
+            numNodeUsage[i].value=12;
+        }
 
     }
 
@@ -35,37 +49,27 @@ function calculatebill(){
         consumption=consumption+ (parseInt(numNodeUsage[i].value)*parseInt(numNodewatt[i].value));
 
     }
-    totwatt=totwatt+(parseInt(numWatt1[0].value))*3;
-    consumption=consumption+(( numUsage1[0].value*numWatt1[0].value)*3)   // consumption added for 3 washroom. total consumption per day.
+    for(let i=0;i<numUsage1.length;i++){
+        totwatt=totwatt+(parseInt(numWatt1[i].value));
+        consumption=consumption+(( numUsage1[i].value*numWatt1[i].value))   // consumption added for 3 washroom. total consumption per day.
+    }
+
     let annualConsumption=consumption*365; // annual consumption.
     consumption=consumption*30;  // consumption of 30 days;
-   
-    // numNodewatt.forEach(element => {
-        
-    //     totwatt=totwatt + parseInt(element.value);
-    //     consumption=consumption+ parseInt(element)
 
-
-        
-    // });
     
     document.querySelector(".billLoad").innerHTML="Your total Load is : "+totwatt+" W";
     document.querySelector(".billConsumption").innerHTML= "Your Monthly Consumption is : "+(consumption)/1000+" KWH";
     document.querySelector(".billAmount").innerHTML="Your Bill Amount is : "+"Rs."+ ((consumption/1000)*5);
     document.querySelector('.annualConsump').innerHTML="Your Annual Consumption is: "+(annualConsumption/1000)+" KWH";
 
-    // const val=document.querySelectorAll('.appliance');
-    // const val1=document.querySelectorAll('#fan1')
-    // console.log(val1);
-    // val.forEach(element => {
-    //     console.log(element.childNodes.textContent)
-        
-    // });
-
 
 }
 
-function myfunction(element)
-{
-    element.remove();
+function deleteAppliance(element){
+    element.parentNode.parentNode.remove();
+}
+
+function addAppliance(element){
+    element.
 }
