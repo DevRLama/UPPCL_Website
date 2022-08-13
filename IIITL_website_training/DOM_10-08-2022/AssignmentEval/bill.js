d8327543564a6fe9118b737aa6d8ed374fd82c66
DevRLama <dev.lama007@gmail.com>
2022-08-13 10:40:14 +0530

    Button and Washroom changes done

--- IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
+++ IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
@@ -1,3 +1,4 @@
+var count=21;
 function initialize(){
     const numNodewatt=document.querySelectorAll(".wattField");
     const numNodeUsage=document.querySelectorAll(".usageField");
@@ -51,25 +52,41 @@ function calculatebill(){
     }
     for(let i=0;i<numUsage1.length;i++){
         totwatt=totwatt+(parseInt(numWatt1[i].value));
-        consumption=consumption+(( numUsage1[i].value*numWatt1[i].value))   // consumption added for 3 washroom. total consumption per day.
+        consumption=consumption+(( numUsage1[i].value*numWatt1[i].value));   // consumption added for 3 washroom. total consumption per day.
     }
-
+    //consumption=consumption+(( numUsage1[0].value*numWatt1[0].value)*3);
+    // totwatt=totwatt+((numWatt1[0].value)*3);
     let annualConsumption=consumption*365; // annual consumption.
     consumption=consumption*30;  // consumption of 30 days;
-
+    
     
     document.querySelector(".billLoad").innerHTML="Your total Load is : "+totwatt+" W";
     document.querySelector(".billConsumption").innerHTML= "Your Monthly Consumption is : "+(consumption)/1000+" KWH";
     document.querySelector(".billAmount").innerHTML="Your Bill Amount is : "+"Rs."+ ((consumption/1000)*5);
     document.querySelector('.annualConsump').innerHTML="Your Annual Consumption is: "+(annualConsumption/1000)+" KWH";
+    if(count==0)
+    {
 
+    }
 
 }
 
 function deleteAppliance(element){
+    
     element.parentNode.parentNode.remove();
+    count=count-1;
+    if(count==0)
+    {
+    document.querySelector(".billLoad").innerHTML="Your total Load is : 0 W";
+    document.querySelector(".billConsumption").innerHTML= "Your Monthly Consumption is : 0 KWH";
+    document.querySelector(".billAmount").innerHTML="Your Bill Amount is : "+"Rs.0";
+    document.querySelector('.annualConsump').innerHTML="Your Annual Consumption is: 0 KWH";
+    }else
+    {
+        calculatebill();
+    }
 }
 
-function addAppliance(element){
-    element.
-}
+// function addAppliance(element){
+//     element.
+// }
